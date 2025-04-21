1b63d41bbd26f9358ba433dadff6e4a10a0d8b66
tas-unn <92320184+tas-unn@users.noreply.github.com>
2025-04-21 15:19:51 +0300

    Update side_bar.dart

--- lib/src/side_bar.dart
+++ lib/src/side_bar.dart
@@ -46,62 +46,51 @@ class SideBar extends StatefulWidget {
   final Widget? footer;
 
   @override
-  _SideBarState createState() => _SideBarState();
+  State<SideBar> createState() => _SideBarState();
 }
 
-class _SideBarState extends State<SideBar> with SingleTickerProviderStateMixin {
+class _SideBarState extends State<SideBar> {
   late double _sideBarWidth;
-  late Widget _child;
-
-  @override
-  void initState() {
-    super.initState();
-    _sideBarWidth = widget.width;
-    _child = Column(
-      children: [
-        if (widget.header != null) widget.header!,
-        Expanded(
-          child: Material(
-            color: widget.backgroundColor,
-            child: ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                return SideBarItem(
-                  items: widget.items,
-                  index: index,
-                  onSelected: widget.onSelected,
-                  selectedRoute: widget.selectedRoute,
-                  depth: 0,
-                  iconColor: widget.iconColor,
-                  activeIconColor: widget.activeIconColor,
-                  textStyle: widget.textStyle,
-                  activeTextStyle: widget.activeTextStyle,
-                  backgroundColor: widget.backgroundColor,
-                  activeBackgroundColor: widget.activeBackgroundColor,
-                  borderColor: widget.borderColor,
-                );
-              },
-              itemCount: widget.items.length,
-              controller: widget.scrollController ?? ScrollController(),
-            ),
-          ),
-        ),
-        if (widget.footer != null) widget.footer!,
-      ],
-    );
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    final mediaQuery = MediaQuery.of(context);
-    _sideBarWidth = min(mediaQuery.size.width * 0.7, widget.width);
-  }
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    _sideBarWidth = min(mediaQuery.size.width * 0.7, widget.width);
+
     return Container(
       width: _sideBarWidth,
-      child: _child,
+      color: widget.backgroundColor,
+      child: Column(
+        children: [
+          if (widget.header != null) widget.header!,
+          Expanded(
+            child: Material(
+              color: widget.backgroundColor,
+              child: ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return SideBarItem(
+                    items: widget.items,
+                    index: index,
+                    onSelected: widget.onSelected,
+                    selectedRoute: widget.selectedRoute,
+                    depth: 0,
+                    iconColor: widget.iconColor,
+                    activeIconColor: widget.activeIconColor,
+                    textStyle: widget.textStyle,
+                    activeTextStyle: widget.activeTextStyle,
+                    backgroundColor: widget.backgroundColor,
+                    activeBackgroundColor: widget.activeBackgroundColor,
+                    borderColor: widget.borderColor,
+                  );
+                },
+                itemCount: widget.items.length,
+                controller: widget.scrollController ?? ScrollController(),
+              ),
+            ),
+          ),
+          if (widget.footer != null) widget.footer!,
+        ],
+      ),
     );
   }
 }
