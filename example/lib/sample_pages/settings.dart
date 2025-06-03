5df9f2198c012fac3d9ff5d2672cd287888b577f
tas-unn <92320184+tas-unn@users.noreply.github.com>
2025-06-03 14:17:09 +0300

    Create settings.dart

---
+++ example/lib/sample_pages/settings.dart
@@ -0,0 +1,22 @@
+// sample_pages/dashboard_page_content.dart
+import 'package:flutter/material.dart';
+
+class SettingsPageContent extends StatelessWidget {
+  const SettingsPageContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Этот виджет теперь просто контент страницы
+    return Container(
+      alignment: Alignment.topLeft,
+      padding: const EdgeInsets.all(10),
+      child: const Text(
+        'SettingsPageContent',
+        style: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 36,
+        ),
+      ),
+    );
+  }
+}
